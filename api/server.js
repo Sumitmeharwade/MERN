4087ea8c7d3b6912fbd64eef52037288dace2e07
sumitmeharwade1@gmail.com <sumitmeharwade1@gmail.com>
2023-12-09 12:14:48 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri,{useNewUrlParser:true})
+const connection=mongoose.connection
+connection.once('open',()=>{
+    console.log("MongoDB is Connected")
+})
+
+const app= express();
+const port = process.env.PORT || 5000
+
+app.use(cors()) //middleware
+app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`server is running on port : ${port}`)
+})
